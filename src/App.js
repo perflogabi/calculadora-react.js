9c9ff81e706b95dc6ea7c13346e9afd1972825c1
Gabriela Florêncio <perflogabi@gmail.com>
2022-09-11 00:20:58 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <h1>Calculadora</h1>  
+      <Calculator/>
     </div>
   );
 }
